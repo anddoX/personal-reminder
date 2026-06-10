2bbcfc2a97a60044a2ebe4b8fbea63a8c5a534ea
anddoX <151229817+anddoX@users.noreply.github.com>
2026-06-10 13:56:58 -0700

    Update ConsoleColors.java

--- ConsoleColors.java
+++ ConsoleColors.java
@@ -1,1 +1,8 @@
+package reminderProgramPackage;
 
+//This is only used for testing, for improved readability
+public class ConsoleColors {
+	public static final String TEXT_RESET  = "\u001B[0m";
+	public static final String TEXT_RED    = "\u001B[31m";
+	public static final String TEXT_GREEN  = "\u001B[32m";
+}
