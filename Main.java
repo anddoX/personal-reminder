9ae5c2c51ca5ea15e5eaf49b159acbaee9a1aa83
anddoX <151229817+anddoX@users.noreply.github.com>
2026-06-10 14:01:53 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+package reminderProgramPackage;
+
+public class Main {
+	public static void main(String[] arg) {
+	ReminderProgramClass reminder = new ReminderProgramClass();
+	reminder.startProgram();
+  }
+}
