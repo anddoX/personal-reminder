8ee17bfa464f4a624b3fa17eefde6094c6da315d
anddoX <151229817+anddoX@users.noreply.github.com>
2026-07-01 22:41:24 -0700

    Create ConsoleColors.java

---
+++ src/ConsoleColors.java
@@ -0,0 +1,6 @@
+//This is only used for testing, for improved readability
+public class ConsoleColors {
+	public static final String TEXT_RESET  = "\u001B[0m";
+	public static final String TEXT_RED    = "\u001B[31m";
+	public static final String TEXT_GREEN  = "\u001B[32m";
+}
